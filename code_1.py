177f83aaf1235aab3b4b52b4b87542affa929f04
Radha Joshi <radha.joshi.1907@gmail.com>
2026-02-12 00:51:08 +0530

    add pycham files containing pla1 practicals 1-5

--- code_1.py
+++ code_1.py
@@ -1,5 +1,2 @@
-# take two numbers from user and print sum.
-n1 = int(input("enter a number :"))
-n2 = int(input("enter another number :"))
-a = n1 + n2
-print("the sum is" , a , ".")
+
+print("hello world")
