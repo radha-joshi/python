c62f3709cf29add91116a6af8d1412991b424b90
Radha Joshi <radha.joshi.1907@gmail.com>
2026-08-08 23:16:37 +0530

    new

--- PycharmProjects/PLA1/practical_12.py
+++ PycharmProjects/PLA1/practical_12.py
@@ -3,34 +3,34 @@
 #Radha Joshi [122-A]
 #line graph
 import matplotlib.pyplot as plt
-x = [1, 2, 3, 4, 5]
-y = [10, 20, 15, 25, 30]
+x = ["Pencil", "Eraser", "Scale", "Scissors", "Notebook"]
+y = [10, 10, 20, 40, 50]
 plt.plot(x, y)
-plt.title("Line Graph")
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
+plt.title("Stationary Price Distribution")
+plt.xlabel("Stationary")
+plt.ylabel("Prices")
 plt.show()
 
 #Radha Joshi [122-A]
 #bar graph
 import matplotlib.pyplot as plt
-x = ['A', 'B', 'C', 'D']
-y = [5, 7, 3, 8]
+x = ['Math', 'Science', 'English', 'Geography']
+y = [60, 70, 50, 90]
 plt.bar(x, y)
-plt.title("Bar Graph")
-plt.xlabel("Categories")
-plt.ylabel("Values")
+plt.title("Academic Performance of Student")
+plt.xlabel("Subjects")
+plt.ylabel("Marks")
 plt.show()
 
 #Radha Joshi [122-A]
 #scatter plot
 import matplotlib.pyplot as plt
-x = [1, 2, 3, 4, 5]
-y = [12, 18, 25, 30, 22]
+x = ["Pencil", "Eraser", "Scale", "Scissors", "Notebook"]
+y = [10, 10, 20, 40, 50]
 plt.scatter(x, y)
-plt.title("Scatter Plot")
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
+plt.title("Stationary Price Distribution")
+plt.xlabel("Stationary")
+plt.ylabel("Prices")
 plt.show()
 
 #Radha Joshi [122-A]
@@ -38,16 +38,16 @@ plt.show()
 import matplotlib.pyplot as plt
 data = [10, 20, 20, 30, 30, 30, 40, 50]
 plt.hist(data)
-plt.title("Histogram")
-plt.xlabel("Values")
+plt.title("Data Frequency Distribution")
+plt.xlabel("Data")
 plt.ylabel("Frequency")
 plt.show()
 
 #Radha Joshi [122-A]
 #pie chart
 import matplotlib.pyplot as plt
-labels = ['A', 'B', 'C', 'D']
-sizes = [25, 35, 20, 20]
-plt.pie(sizes, labels=labels)
-plt.title("Pie Chart")
+Subjects = ['IoT', 'DS', 'OOP', 'CAO']
+Proportion = [25, 35, 20, 20]
+plt.pie(Proportion, labels=Subjects)
+plt.title("Elective Subject Preferences")
 plt.show()
